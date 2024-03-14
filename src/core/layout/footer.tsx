eac703fc4a9198b71b51ea083751711f2d9df2f8
unknown <brunohc2016@gmail.com>
2024-03-14 19:49:42 -0300

    feat: changing project layout

--- src/core/layout/footer.tsx
+++ src/core/layout/footer.tsx
@@ -2,7 +2,7 @@ import { Flex, Text } from "@chakra-ui/react";
 
 export function Footer() {
   return (
-    <Flex width="full" height="14" alignItems="center" justifyContent="center" backgroundColor="#3A506B">
+    <Flex width="full" height="14" alignItems="center" justifyContent="center" backgroundColor="#C7C8CC">
       <Text fontWeight="bold" color="white">API used: fakestoreapi.com</Text>
     </Flex>
   );
--- src/core/layout/index.tsx
+++ src/core/layout/index.tsx
@@ -7,7 +7,7 @@ export function MainLayout() {
   return (
     <Box>
       <Header />
-      <Flex width="full" minHeight="calc(100vh - 112px)" bgGradient="linear(to-b, #232D3F, #3A506B)">
+      <Flex width="full" minHeight="calc(100vh - 112px)" bgGradient="linear(to-b, #B4B4B8, #C7C8CC)">
         <Outlet />
       </Flex>
       <Footer />
